bc13cfe4436029d10fd5c1300d6e7b82d9f94216
qasem-moh <2171246@s.aou.edu.jo>
2021-08-11 09:53:58 +0300

    fix2

---
+++ src/middleware/validator.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = person = (userName) => {
+  return (req, res, next) => {
+    req.personName = userName;
+    if (typeof userName == 'string') {
+
+      next();
+    }
+    else
+      next(`sorry the input not string : ' ${userName} '`);
+  };
+};
